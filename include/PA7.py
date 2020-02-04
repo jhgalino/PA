288c6f3276f49bf88cc1d633b3616d651f67a442
John Henry Galino <jhgalino@gmail.com>
2020-02-05 01:32:39 +0800

    removed searhforAddend

--- include/PA7.py
+++ include/PA7.py
@@ -1,7 +1,7 @@
 inpList = sorted([int(x) for x in input().split()])
 testCases = int(input())
 sums = []
-results = set([])
+results = set()
 
 for i in range(testCases):
     sums.append(int(input()))
@@ -13,22 +13,15 @@ def removeItem(listToSearchIn: list, indexToRemove: int):
 
 def checkForAddends(listToSearchIn: list, initialVal: int, total: int):
     result = list(set(list(filter(lambda x: x + initialVal == total, listToSearchIn))))
-    return result[0]
+    if len(result) > 0:
+        return result[0]
 
-
-# def searchForAddend(listToSearchIn: list, initialVal: int, totalToSearch: int):
-#     for i, v in enumerate(listToSearchIn):
-#         if initialVal + v == totalToSearch:
-#             editedList = removeItem(listToSearchIn, i)
-#             return v, editedList
-
-
-# def iterateThroughList(listToSearchIn: list, totalToSearch: int):
-#     for i in listToSearchIn:
 
 for i in sums:
     for j in inpList:
-        results.append(sorted((j, checkForAddends(inpList, j, i))))
+        results.add(tuple(sorted((j, checkForAddends(inpList, j, i)))))
+    results = sorted(list(results))
     print(len(results))
     for k in results:
-        print(" ".join(k))
+        print(" ".join(str(i) for i in k))
+    results = set()
